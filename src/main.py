6c39505b33ce3d5670ec70207d579b4804f691e4
Qian ying Wong <qywong@uci.edu>
2025-01-29 16:28:08 +0000

    Updated version

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
  # Current data and time
+import datetime
+
+
+current_time = datetime.datetime.now()
+file_path = "../docs/version.md"
+
+with open(file_path, "w") as file:
+    file.write(f" # Version information\n\n")
+    file.write(f"{current_time}")
